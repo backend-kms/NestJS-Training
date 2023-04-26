698d222096978d69c43c4675ec3e60c77ebb85e3
Kimminsu <kms@kristinkorea.com>
2023-04-26 09:52:55 +0900

    config 설정 관련 작업

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
+import * as config from 'config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
+  const options = new DocumentBuilder()
     .setTitle('Swagger Title')
     .setDescription('The test API description')
     .setVersion('1.0')
     .addTag('tests')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('swagger', app, document); // localhost:3000/swagger
 
-  await app.listen(3000);
+  const serverConfig = config.get('server')
+  const port = serverConfig.port;
+
+  await app.listen(port);
+  Logger.log(`Application running on port ${port}`)
 }
 bootstrap();
